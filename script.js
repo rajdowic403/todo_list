7855ee0e0d0ce0ea94f9f8bb1298ca9cea9f86da
Konrad Mazur <konrad.mazurbt@gmail.com>
2022-06-20 17:52:34 +0200

    my todo list

---
+++ script.js
@@ -0,0 +1,25 @@
+
+const add = () => {
+    let tasksadd = document.querySelector("#taskAdd").value;
+    
+    const task2 = document.querySelector("#tasks");
+    
+    const task = document.createElement("div");
+        
+        task.classList.add("task");
+        task.innerHTML = tasksadd;
+
+    const deleted = document.createElement("button");
+        
+        deleted.classList.add("deleted");
+        deleted.innerHTML = "X";
+        deleted.addEventListener("click", e =>{
+        
+            task.remove();
+
+        })
+
+    task.appendChild(deleted);
+    task2.appendChild(task);
+   
+}
